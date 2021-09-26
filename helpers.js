f2b1ba3501ed4fbbc7fb79e855f362b13f02ebb7
Simo <89608450+Gizmo2003M@users.noreply.github.com>
2021-09-26 12:42:18 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *SIMO*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *Simo*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *Simo*:  ```" + msg + "```"
 }
 
 
